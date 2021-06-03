4539cb82112bafdfa5b7800d5bde750e284d28f7
Amina Eltahir <amina.a.ibrahim22@gmail.com>
2021-06-02 18:25:36 -0600

    scrollup

--- js/scrollup.js
+++ js/scrollup.js
@@ -1,15 +1,16 @@
-var btn = $('#button');
-
-$(window).scroll(function() {
-  if ($(window).scrollTop() > 300) {
-    btn.addClass('show');
+$(document).ready(function() {
+  $(window).scroll(function() {
+  if ($(this).scrollTop() > 20) {
+  $('#toTopBtn').fadeIn();
   } else {
-    btn.removeClass('show');
+  $('#toTopBtn').fadeOut();
   }
-});
-
-btn.on('click', function(e) {
-  e.preventDefault();
-  $('html, body').animate({scrollTop:0}, '300');
-});
-
+  });
+  
+  $('#toTopBtn').click(function() {
+  $("html, body").animate({
+  scrollTop: 0
+  }, 1000);
+  return false;
+  });
+  });
